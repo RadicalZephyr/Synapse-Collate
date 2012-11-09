9d02298dd0dfde00a185b7b8597b89b2293c3d79
Geoff Shannon <earthlingzephyr@gmail.com>
2012-11-08 16:28:18 -0800

    Stub test, initial implementation and test helper file

--- src/main/java/org/sagebionetworks/mapred/mapper/FileNameToDate.java
+++ src/main/java/org/sagebionetworks/mapred/mapper/FileNameToDate.java
@@ -1,5 +1,9 @@
 package org.sagebionetworks.mapred.mapper;
 
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -7,4 +11,21 @@ import org.apache.hadoop.mapreduce.Mapper;
 public class FileNameToDate
 	extends Mapper<LongWritable, Text, Text, Text> {
 
+	public Pattern s3Pattern = Pattern.compile("s3://[.\\w]+/(\\w+/\\w)/[-\\w]/([-\\w.]+)");
+	public Pattern filePattern = Pattern.compile("[-\\w]+\\.(\\d{4}-\\d{2}-\\d{2})-\\d{2}-\\d{2}\\.gz");
+
+	public void map(LongWritable key, Text value, Context context) 
+		throws IOException, InterruptedException {
+		String line = value.toString();
+
+		Matcher s3Matcher = s3Pattern.matcher(line);
+		Matcher fileMatcher = filePattern.matcher(s3Matcher.group(2));
+
+		StringBuilder keyBuilder = new StringBuilder();
+		keyBuilder.append(s3Matcher.group(1));
+		keyBuilder.append("-");
+		keyBuilder.append(fileMatcher.group());
+
+		context.write(new Text(keyBuilder.toString()), new Text(s3Matcher.group()));
+	}
 }
---
+++ src/test/java/org/sagebionetworks/mapred/mapper/FileNameToDateTest.java
@@ -0,0 +1,13 @@
+package org.sagebionetworks.mapred.mapper;
+
+public class FileNameToDateTest {
+
+	
+	public void testS3Pattern() {
+		
+	}
+	
+	public void testFilePattern() {
+		
+	}
+}
